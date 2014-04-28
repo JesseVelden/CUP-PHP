7095b8853d1c4505897deb1f5c899e8388566268
Jesse van der Velden <megacookie@riseup.net>
2014-04-28 11:46:08 +0200

    Create api-example.php

---
+++ api-example.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(E_ERROR);
+require "api.php";
+$filter='Hoek'; //Put here the name
+$password='1234'
+$schoolUrl='ed.cupweb6.nl'; //the URL of your School's CUP website.
+$names=cupphp\getNames($filter,$schoolUrl);
+var_dump($names);
+echo '<br><br>';
+var_dump(cupphp\getTimeTable($names->names[0]->username,$password,$schoolUrl,$names->sessionId)); //Get the Timetable of the first User that can be called with the name: 'Hoek' with using the password: '1234'
+?>
