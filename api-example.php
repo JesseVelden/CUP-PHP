6cc052f2e87680a634fea7a7c794f84d0772fdd6
Jesse van der Velden <megacookie@riseup.net>
2014-05-12 16:31:54 +0200

    Updated api-example.php because of changed api.php

--- api-example.php
+++ api-example.php
@@ -7,5 +7,5 @@ $schoolUrl='ed.cupweb6.nl'; //the URL of your School's CUP website.
 $names=cupphp\getNames($filter,$schoolUrl);
 var_dump($names);
 echo '<br><br>';
-var_dump(cupphp\getTimeTable($names->names[0]->username,$password,$schoolUrl,$names->sessionId)); //Get the Timetable of the first User that can be called with the name: 'Hoek' with using the password: '1234'
+var_dump(cupphp\getTimeTable($names->names[0]->username,$password,$schoolUrl,$names->sessionId,$names->eventvalidation)); //Get the Timetable of the first User that can be called with the name: 'Hoek' with using the password: '1234'
 ?>
